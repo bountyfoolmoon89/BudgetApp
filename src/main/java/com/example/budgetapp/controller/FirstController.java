5e47a6f856e1198de938a0c32a3fbe2034a133ca
Егор <bountyfoolmoon89@yandex.com>
2023-01-28 19:00:18 +0300

    start

---
+++ src/main/java/com/example/budgetapp/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.budgetapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello, World!";
+    }
+}
